d86a7417ddbb7fbd477f8c17abfc557471ea13cd
wedinusda <110817808+wedinusda@users.noreply.github.com>
2022-08-08 16:18:15 +0700

    Add files via upload

---
+++ database/gcvirus.js
@@ -0,0 +1,65 @@
+const gcvirus = (prefix) => {
+	return`
+	ALL GROUP VIRUS
+▰▱▰▱▰▱▰▱▰▱▰▱▰▱
+┏━━━━━━━━━━━━━━━━━⊱
+┣❏〘 ༒✪͜͡★➤⃨⃞⃟𝙱𝚄ͭ𝙶ͤ𝙱ⷶ𝙾ͫ𝚃➤⃨⃞⃟★፝͜͡✪༒ 〙
+┣⃰͜͡✦҈͜͡➳ https://chat.whatsapp.com/DkbADkJEzSp9ziw9oqLUiE
+┗━━━━━━━━━━━━━━━━━⊱
+┏━━━━━━━━━━━━━━━━━⊱
+┣❏〘 👑҈꙰⃝Ꮶ♔︎N҈G҈༒Ṽḯ℘ʊֆ👑҈꙰⃝〽️ 〙
+┣⃰͜͡✦҈͜͡➳ https://chat.whatsapp.com/CMWBwf3NRZD4OxFjHJG1UK
+┗━━━━━━━━━━━━━━━━━⊱
+┏━━━━━━━━━━━━━━━━━⊱
+┣❏〘 ✘シ𝘉𝘜𝘛𝘛𝘖𝘕 ƭ૨เρℓε🎭✘シ 〙
+┣⃰͜͡✦҈͜͡➳ https://chat.whatsapp.com/HWxFSgf4qOdELaekkp0ANX
+┗━━━━━━━━━━━━━━━━━⊱
+┏━━━━━━━━━━━━━━━━━⊱
+┣❏〘 ☠シρ૨εƒเx 𝘽𝙐𝙏𝙏𝙊𝙉𝘤𝘰𝘮𝘣𝘪𝘯𝘦🎭 〙
+┣⃰͜͡✦҈͜͡➳ https://chat.whatsapp.com/K7bOVjVu4dL7soW7tsbgth
+┗━━━━━━━━━━━━━━━━━⊱
+┏━━━━━━━━━━━━━━━━━⊱
+┣❏〘 ✘🎭𝙆𝙄𝙉𝙂 𝑏𝑢𝑔𝑏𝑜𝑡Zシ🎭 〙
+┣⃰͜͡✦҈͜͡➳ https://chat.whatsapp.com/C7tscQhgM9IAxD8belaZX9
+┗━━━━━━━━━━━━━━━━━⊱
+┏━━━━━━━━━━━━━━━━━⊱
+┣❏〘 ঔৣ͜͡✪❦VI®️US❦✿͜͡🇲🇨 〙
+┣⃰͜͡✦҈͜͡➳ https://chat.whatsapp.com/Ee7vYMJrI6o6ZLQvb5Ctac
+┗━━━━━━━━━━━━━━━━━⊱
+┏━━━━━━━━━━━━━━━━━⊱
+┣❏〘 💀✰͜͡❖𝐯ᷢ𝐢𝐫ͦ𝐮ͦ𝐬ᷟ 𝟱𝟬𝟱❖͜͡✰💀 〙
+┣⃰͜͡✦҈͜͡➳ https://chat.whatsapp.com/FlZOKWk2oMr7UivaOxUpWk
+┗━━━━━━━━━━━━━━━━━⊱
+┏━━━━━━━━━━━━━━━━━⊱
+┣❏〘 ꧁ℭ℟Åℤ¥༒ཞ ơ ơ ɱ  ۷ ı ཞ ų ʂ 〙
+┣⃰͜͡✦҈͜͡➳ https://chat.whatsapp.com/GhMhv6m0pbmEmZAuMsN4mn
+┗━━━━━━━━━━━━━━━━━⊱
+┏━━━━━━━━━━━━━━━━━⊱
+┣❏〘 𔕋ᴮᴵᵃᵑᶻ⏧𝐕𝐈𝐑𝐔𝐒⏧ᶦᵖᵐᵃᵑᶻ𔕋 〙
+┣⃰͜͡✦҈͜͡➳ https://chat.whatsapp.com/IH9kyt2zowXAQJiOCo1OjM
+┗━━━━━━━━━━━━━━━━━⊱
+┏━━━━━━━━━━━━━━━━━⊱
+┣❏〘 😈👿✿͜͡ঔৣ⃢✪WP$ VłƦU$ৡৢั🥵🥶🤡░ 〙
+┣⃰͜͡✦҈͜͡➳ https://chat.whatsapp.com/Jh0EznQhxbkCHIre0L2bTr
+┗━━━━━━━━━━━━━━━━━⊱
+┏━━━━━━━━━━━━━━━━━⊱
+┣❏〘 ꧁𝐕ͬ𝗶ͦ𝗿ͦ𝘂ͫ𝘀 𝗟𝗼ᷧ𝗺ᷠ𝐛ᷧ𝗼ᷜ𝗸꧂ 〙
+┣⃰͜͡✦҈͜͡➳ https://chat.whatsapp.com/DQWBTR8ClyiBZzjhtOFOP0
+┗━━━━━━━━━━━━━━━━━⊱
+┏━━━━━━━━━━━━━━━━━⊱
+┣❏〘 💀꙲◉ͬ𝒇ͦ𝒓ͦ𝒆ͫ𝒆𝒅ͮ𝒐ͥ𝒐ͬ𝒎ͧ๏ᷤ💀꙲ 〙
+┣⃰͜͡✦҈͜͡➳ https://chat.whatsapp.com/GMAwtzdneefAdOegAbUvu7
+┗━━━━━━━━━━━━━━━━━⊱
+┏━━━━━━━━━━━━━━━━━⊱
+┣❏〘 DARK LINK KING 🐲 〙
+┣⃰͜͡✦҈͜͡➳ https://chat.whatsapp.com/LZDK7wAOMw0LDXeCgv6FkY
+┗━━━━━━━━━━━━━━━━━⊱
+┏━━━━━━━━━━━━━━━━━⊱
+┣❏〘 🎭シяøøღ 𝙁𝙄𝙍𝙐𝙎 𝘤𝘪𝘯𝘵𝘢🎭✘ 〙
+┣⃰͜͡✦҈͜͡➳ https://chat.whatsapp.com/BW3oxa2O6lQ9JwruziGO8W
+┗━━━━━━━━━━━━━━━━━⊱
+▰▱▰▱▰▱▰▱▰▱▰▱▰▱
+©️𝚃𝙷𝙴 𝙹𝙾 𝙱𝙾𝚃
+`
+}
+exports.gcvirus = gcvirus
